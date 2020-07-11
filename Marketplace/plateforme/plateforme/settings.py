5a777b009b478f66a38660023ac8356559256d95
Djewiet <64140179+Djewiet@users.noreply.github.com>
2020-07-11 16:35:27 +0200

    Update settings.py

--- Marketplace/plateforme/plateforme/settings.py
+++ Marketplace/plateforme/plateforme/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '37czx#azyob=otote4oa*hc3vx_cw4*^!yrstgxq$@8h6vhk#5'
+#
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
